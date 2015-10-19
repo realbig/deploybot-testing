1a24df7e872883882db34ea9030205fc085ab385
joelworsham <joelworsham@gmail.com>
2015-10-19 16:00:34 -0400

    Update to deploy to production and staging

--- bin/deploybot.sh
+++ bin/deploybot.sh
@@ -1,3 +1,12 @@
 #!/usr/bin/env bash
 
-curl -XPOST 'https://real-big-marketing.deploybot.com/webhook/deploy?env_id=49886&secret=8ec8e965c5d3d98aba9bd1b0d863c44881a096bd36188345'
+if [ $TRAVIS_BRANCH == 'master' ]; then
+
+    # Deploy production
+    curl -XPOST 'https://real-big-marketing.deploybot.com/webhook/deploy?env_id=49842&secret=8ec8e965c5d3d98aba9bd1b0d863c44881a096bd36188345'
+
+elif [ $TRAVIS_BRANCH == 'develop' ]; then
+
+    # Deploy staging
+    curl -XPOST 'https://real-big-marketing.deploybot.com/webhook/deploy?env_id=49886&secret=8ec8e965c5d3d98aba9bd1b0d863c44881a096bd36188345'
+fi
